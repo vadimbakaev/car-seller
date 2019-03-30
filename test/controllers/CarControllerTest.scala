21464c6df9dd3183a28c93a48f0a37e968f269f7
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-30 22:52:23 +0100

    Update CarController test, add Conflict case

--- test/controllers/CarControllerTest.scala
+++ test/controllers/CarControllerTest.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import mappers.CarRequest2CarInfo
 import models.FuelType
-import models.commands.AddCarResult
+import models.commands.{AddCarResult, FailedResult}
 import models.request.CarRequest
 import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.ParallelTestExecution
@@ -16,7 +16,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers.{status, stubControllerComponents, _}
 import play.mvc.Http.HeaderNames
 import services.CommandHandler
-
+import CarControllerTest._
 import scala.concurrent.{ExecutionContext, Future}
 
 class CarControllerTest
@@ -31,31 +31,26 @@ class CarControllerTest
     val commandHandler: CommandHandler = mock[CommandHandler]
 
     val controller = new CarController(stubControllerComponents(), new CarRequest2CarInfo(), commandHandler)
+
+    def executeGETRequest(body: JsValue): Future[Result] = {
+      val request: FakeRequest[JsValue] = FakeRequest(POST, "/public/v1/car")
+        .withBody(body)
+        .withHeaders(HeaderNames.CONTENT_TYPE -> ContentTypes.JSON)
+      controller.add.apply(request)
+    }
   }
 
   "CarController POST" should {
     "on add return BadRequest when request is invalid, empty json" in new Fixture {
-      val body: JsValue = Json.obj()
-      val request: FakeRequest[JsValue] = FakeRequest(POST, "/public/v1/car")
-        .withBody(body)
-        .withHeaders(HeaderNames.CONTENT_TYPE -> ContentTypes.JSON)
-      val addCarResponse: Future[Result] = controller.add.apply(request)
+      val body: JsValue                  = Json.obj()
+      val addCarResponse: Future[Result] = executeGETRequest(body)
 
       status(addCarResponse) mustBe BAD_REQUEST
     }
 
     "on add return BadRequest when request is invalid" in new Fixture {
-      val body: JsValue = Json.obj(
-        "id"    -> "18f126d7-708b-4640-83f2-7916b9ad0531",
-        "title" -> "Audi A4 Avant",
-        "fuel"  -> "D",
-        "price" -> 7000,
-        "new"   -> false
-      )
-      val request: FakeRequest[JsValue] = FakeRequest(POST, "/public/v1/car")
-        .withBody(body)
-        .withHeaders(HeaderNames.CONTENT_TYPE -> ContentTypes.JSON)
-      val addCarResponse: Future[Result] = controller.add.apply(request)
+      val body: JsValue                  = Json.toJson(InvalidCarRequest)
+      val addCarResponse: Future[Result] = executeGETRequest(body)
 
       status(addCarResponse) mustBe BAD_REQUEST
       contentAsJson(addCarResponse) mustBe Json.obj(
@@ -64,29 +59,30 @@ class CarControllerTest
       )
     }
 
-    "on add return Created" in new Fixture {
+    "on add return Conflict when car already created" in new Fixture {
+      when(commandHandler.handle(*)).thenReturn(Future.successful(FailedResult))
 
+      val body: JsValue                  = Json.toJson(ValidCarRequest)
+      val addCarResponse: Future[Result] = executeGETRequest(body)
+
+      status(addCarResponse) mustBe CONFLICT
+    }
+
+    "on add return Created" in new Fixture {
       val id = "d5449989-95b1-4b0b-a94f-eb653d1171d2"
       when(commandHandler.handle(*)).thenReturn(Future.successful(AddCarResult(id)))
 
-      private val body: JsValue = Json.toJson(
-        CarRequest(
-          UUID.randomUUID(),
-          "Audi A4 Avant",
-          FuelType.Diesel,
-          7000,
-          `new` = true,
-          None,
-          None
-        )
-      )
-      val request: FakeRequest[JsValue] = FakeRequest(POST, "/public/v1/car")
-        .withBody(body)
-        .withHeaders(HeaderNames.CONTENT_TYPE -> ContentTypes.JSON)
-      val addCarResponse: Future[Result] = controller.add.apply(request)
+      val body: JsValue                  = Json.toJson(ValidCarRequest)
+      val addCarResponse: Future[Result] = executeGETRequest(body)
 
       status(addCarResponse) mustBe CREATED
       header(HeaderNames.LOCATION, addCarResponse) mustBe Some(s"/public/v1/car/$id")
     }
   }
+}
+
+object CarControllerTest {
+  val ValidCarRequest: CarRequest =
+    CarRequest(UUID.randomUUID(), "Audi A4 Avant", FuelType.Diesel, 7000, `new` = true, None, None)
+  val InvalidCarRequest: CarRequest = ValidCarRequest.copy(`new` = false)
 }
