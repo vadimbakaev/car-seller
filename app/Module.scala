8048a8ed6af0ee8bb42c88adb247b1c66eae7fb2
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-31 21:26:56 +0200

    Remove unused binding in Module

--- app/Module.scala
+++ app/Module.scala
@@ -2,17 +2,15 @@ import java.time.Clock
 
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
-import common.ErrorHandler
 
 class Module extends AbstractModule {
 
   override def configure(): Unit = {
+
     bind(classOf[Clock])
       .annotatedWith(Names.named("clock"))
       .toInstance(Clock.systemUTC())
 
-    bind(classOf[ErrorHandler])
-      .asEagerSingleton()
   }
 
 }
