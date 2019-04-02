1b7a0c9b8b4dde487e48e29ae24d3ff93e1bb537
Vadim Bakaev <bakaev.vad@gmail.com>
2019-04-02 20:41:05 +0200

    Add old car test case in CarAdvertRequestTest

--- test/controllers/requests/CarAdvertRequestTest.scala
+++ test/controllers/requests/CarAdvertRequestTest.scala
@@ -1,17 +1,18 @@
 package controllers.requests
 
+import java.time.Instant
 import java.util.UUID
 
+import controllers.common.FuelType
+import controllers.requests.CarAdvertRequestTest._
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.Json
-import CarAdvertRequestTest._
-import controllers.common.FuelType
 
 class CarAdvertRequestTest extends PlaySpec {
 
   "CarAdvertRequest" should {
 
-    "fail to deserialize from invalid json" in {
+    "fail to deserialize from invalid json, missing field for old car advert" in {
       Json
         .obj(
           "id"    -> Id,
@@ -24,7 +25,7 @@ class CarAdvertRequestTest extends PlaySpec {
         .isError mustBe true
     }
 
-    "ingnore addition field for new car advert" in {
+    "ignore addition field for new car advert" in {
       Json
         .obj(
           "id"                 -> Id,
@@ -45,6 +46,31 @@ class CarAdvertRequestTest extends PlaySpec {
           `new` = true,
           None,
           None
+        )
+      )
+    }
+
+    "map addition field for old car advert correctly" in {
+      Json
+        .obj(
+          "id"                 -> Id,
+          "title"              -> "Audi A4 Avant",
+          "fuel"               -> "D",
+          "price"              -> 7000,
+          "new"                -> false,
+          "mileage"            -> 99000,
+          "first registration" -> "2017-07-10T05:33:44.914Z"
+        )
+        .validate[CarAdvertRequest]
+        .asOpt mustBe Some(
+        CarAdvertRequest(
+          Id,
+          "Audi A4 Avant",
+          FuelType.Diesel,
+          7000,
+          `new` = false,
+          Some(99000),
+          Some(Instant.parse("2017-07-10T05:33:44.914Z"))
         )
       )
     }
